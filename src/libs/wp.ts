a9ffe5df65e6f72d1e5ad357470a29f5721ad308
jorge-devtai <jorge.deleon@taiarts.com>
2025-06-24 17:14:49 +0200

    Update CardArtist

--- src/libs/wp.ts
+++ src/libs/wp.ts
@@ -1,18 +1,22 @@
 const domain = import.meta.env.URL_WP_DOMAIN;
 const apiUrl = `${domain}/wp-json/wp/v2`;
 
-export const getArtistInfo = async ({ perPage =  10 }: {perPage?: number}) => {
-  const res = await fetch(`${apiUrl}/artista?slug=${perPage}&_embed`);
-  if (!res.ok) throw new Error('Failed to fetch data');
+export const getArtistInfo = async ({ perPage =  3 }: {perPage?: number} = {}) => {
+  const res = await fetch(`${apiUrl}/artista?per_page=${perPage}`);
+  if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+  console.log(res)
 
   const results = await res.json();
-  if (!results.length) throw new Error('No Artists found')
+  console.log(results)
+  if (!Array.isArray(results) || !results.length) throw new Error('No Artists found')
     
     const artists = results.map((artist: any) => {
-      const { title: {redered: title }} = artists
-      return { title }
+      const { title: {rendered: artistName } } = artist;
+      
+      return { artistName }
     })
 
-    return results
+    return artists
 }
   
